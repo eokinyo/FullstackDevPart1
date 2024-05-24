0ef18cb45399ac8e17f901402b056fc22269278f
eokinyo <50834185+eokinyo@users.noreply.github.com>
2024-05-24 11:15:41 +0300

    Update App.jsx
    Studies before ex1 completed.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -93,17 +93,48 @@ const Hello = (props) => {
   )
 }
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
 const App = () => {
   const name = 'Peter'
   const age = 10
 
-  return (
+  /*return (
     <div>
       <h1>Greetings</h1>
       <Hello name= 'Maya' age={26 + 10} />
       <Hello name={name} age={age} />
+      <Footer />
+    </div>
+  )*/
+/*Other than just using the div elements to define a react component. We can also use array components as below*/
+  return [
+    <h1>Greetings</h1>,
+    <Hello name='Maya' age={26+10} />,
+    <Footer />
+  ]
+}
+
+/*Do not render objects */
+/*const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
     </div>
   )
-}
+}*/
+
 
 export default App
